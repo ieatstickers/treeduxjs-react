bfcc6ad0081dbe44055d661a2f5b4fc1e6cbece0
mikeburke <mikeburke.84@protonmail.com>
2025-05-21 23:06:09 +0100

    - Updated dist files

--- dist/src/Type/StateNodeHooks.d.ts
+++ dist/src/Type/StateNodeHooks.d.ts
@@ -1,5 +1,5 @@
 import { StateNodeInterface } from "treeduxjs/dist/src/Type/StateNodeInterface";
-export type StateNodeHooks<StateNode extends StateNodeInterface<any>> = {
+export type StateNodeHooks<StateNode extends StateNodeInterface<any, any>> = {
     [P in keyof Omit<StateNode, 'get' | 'subscribe'>]: StateNode[P];
 } & (StateNode extends {
     get: (...args: any[]) => any;
